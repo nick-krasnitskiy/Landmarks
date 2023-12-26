216c95ed516a9a794760401d48bdbfc7425e527d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-12-26 11:01:24 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nick Krasnitskiy on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
