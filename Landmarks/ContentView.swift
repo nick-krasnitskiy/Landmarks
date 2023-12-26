ebcee84281251ed513759f13cafd11ed88eb8dd0
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-12-26 14:55:53 +0300

    Compose the detail view.

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,6 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
@@ -20,8 +27,17 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            
             .padding()
+
+            Spacer()
         }
     }
 }
