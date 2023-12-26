4cc8ef2e8b62a85203b8b71cf95d0368d3560ab9
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-12-26 14:28:23 +0300

    Create a custom image view.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Nick Krasnitskiy on 26.12.2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
