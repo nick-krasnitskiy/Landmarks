a5871371e8bbd60003e4f6d9d56a2a1d8fd49395
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-12-27 12:49:09 +0300

    Create the row view.

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Nick Krasnitskiy on 27.12.2023.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
