c957aa7ec467eaa94d8667072f2d5818f2d48f62
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-03 21:35:14 +0300

    Add navigation between sections.

--- Landmarks/Views/Categories/CategoryItem.swift
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -13,10 +13,12 @@ struct CategoryItem: View {
     var body: some View {
         VStack(alignment: .leading) {
             landmark.image
+                .renderingMode(.original)
                 .resizable()
                 .frame(width: 185, height: 185)
                 .cornerRadius(5)
             Text(landmark.name)
+                .foregroundStyle(.primary)
                 .font(.caption)
         }
         .padding(.leading, 15)
