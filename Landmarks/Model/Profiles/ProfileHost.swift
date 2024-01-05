ae55c406183523c74ed49a20f26da026ef7d64d6
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-05 08:09:36 +0300

    Display a user profile.

---
+++ Landmarks/Model/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Nick Krasnitskiy on 05.01.2024.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
